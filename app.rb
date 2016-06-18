d8500ac5b8b51c80fc302b9d6270a17b5e28841e
Anne Johnson <annekjohnson23@yahoo.com>
2016-06-18 12:27:30 -0400

    Allow user to type in common name (not just species name)

--- app.rb
+++ app.rb
@@ -3,29 +3,79 @@ require 'net/http'
 require 'json'
 
 class GbifClient
-  def get_occurrences(species_name)
+  def get_species_names(common_name)
+    results = get_results(species_name_path(common_name))
+    get_name_data(results)
+  end
+
+  def get_species_occurrences(species_name)
+    results = get_results(species_occurrences_path(species_name))
+    get_occurrence_data(results)
+  end
+
+  private
+
+  def get_name_data(results)
+    results = results.map do |result|
+      {
+        species_name: result['species'],
+        common_name: get_vernacular_name(result)
+      }
+    end
+    results.select { |result| result[:common_name] }.
+            uniq { |result| result[:species_name] }
+  end
+
+  def get_vernacular_name(result)
+    result['vernacularNames'].select { |h| h['language'] == 'eng' }.
+                              map { |h| h['vernacularName'] }.
+                              first
+  end
+
+  def get_occurrence_data(results)
+    results.map do |result|
+      {
+        species_name: result['species'],
+        latitude: result['decimalLatitude'],
+        longitude: result['decimalLongitude'],
+        common_name: result['vernacularName']
+      }
+    end.select do |result|
+      [:species_name, :latitude, :longitude].all? { |k| result[k] }
+    end
+  end
+
+  def get_results(path)
     begin
       JSON.parse(
-        make_search_request(species_name)
+        Net::HTTP.get(base_url, path)
       )['results']
     rescue
       []
     end
   end
 
-  private
+  def species_name_path(common_name)
+    '/v1/species/search?' +
+      URI.encode_www_form(
+        q: common_name,
+        nameType: 'SCIENTIFIC',
+        rank: 'SPECIES',
+        status: 'ACCEPTED',
+        limit: 50
+      )
+  end
 
-  def make_search_request(species_name)
-    Net::HTTP.get(base_url, search_path(species_name))
+  def species_occurrences_path(species_name)
+    '/v1/occurrence/search?' +
+      URI.encode_www_form(
+        scientificName: species_name,
+        limit: 20
+      )
   end
 
   def base_url
     'api.gbif.org'
-  end
-
-  def search_path(search_term)
-    '/v1/occurrence/search?' +
-      URI.encode_www_form('scientificName' => search_term, limit: 50)
   end
 end
 
@@ -34,14 +84,10 @@ get '/' do
 end
 
 get '/search.json' do
-  occurrences = GbifClient.new.get_occurrences(params['species'])
-  lat_long_data = occurrences.map do |occurrence|
-    {
-      species: occurrence['species'],
-      latitude: occurrence['decimalLatitude'],
-      longitude: occurrence['decimalLongitude']
-    }
-  end
+  api_client = GbifClient.new
+
+  species_name = api_client.get_species_names(params['commonName']).first[:species_name]
+  lat_long_data = api_client.get_species_occurrences(species_name)
 
   lat_long_data.to_json
 end
